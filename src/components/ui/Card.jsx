7705362530b488ab98e73fa24f7e79065d5a5b27
cheruab <cheruabmelkamu693@gmail.com>
2026-03-10 00:19:20 +0300

    card compoment

---
+++ src/components/ui/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card({ children, className = '' }) {
+  return (
+    <div className={`
+      bg-[#0a0a0a] border border-[#222]
+      rounded-[6px] p-5
+      ${className}
+    `}>
+      {children}
+    </div>
+  )
+}
